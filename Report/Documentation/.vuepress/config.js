4678ba5e3bd57613b16dc5a218fd3421760961e9
joe <joe@joebailey.xyz>
2019-03-20 13:44:01 +0000

    Added sidebar in Vuepress Documentation

--- Report/Documentation/.vuepress/config.js
+++ Report/Documentation/.vuepress/config.js
@@ -1,3 +1,68 @@
 module.exports = {
-    base: "/Documentation/.vuepress/dist/"
+    base: "/Documentation/.vuepress/dist/",
+    themeConfig: {
+
+        displayAllHeaders: true,
+        sidebar: [
+            {
+                title: 'Overview',
+                collapsable: true,
+                children: [
+                    '/'
+                ]
+            },
+
+            {
+                title: 'Background',
+                collapsable: true,
+                children: [
+                    '/Background/Background.md'
+                ]
+            },
+
+
+            {
+                title: 'Wire Frames',
+                children: [
+                    '/Wire_Frames/Wire_Frames.md'
+                ]
+            },
+
+            {
+                title: 'Colour Choices',
+                children: [
+                    '/Colour_Choices/Colour_Choices.md'
+                ]
+            },
+
+            {
+                title: 'Accessibility',
+                children:  [
+                    '/Accessibility/Accessibility.md'
+                ]
+            },
+
+
+            {
+                title: 'GDPR',
+                children:  [
+                    '/GDPR/GDPR.md'
+                ]
+            },
+
+            {
+                title: 'Distance Selling',
+                children:  [
+                    '/Distance_Selling/Distance_Selling.md'
+                ]
+            },
+
+            {
+                title: 'Evaluation',
+                children: [
+                    '/Evaluation/Evaluation.md'
+                ]
+            }
+        ]
+    }
 };
